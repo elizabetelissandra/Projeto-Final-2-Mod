dbb16bcbbdab9e5f6e6e1667a9acc7a080684a95
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-05-07 19:51:00 -0300

    create .gitignore and BookingModel.ts

---
+++ src/models/BookingModel.ts
@@ -0,0 +1,18 @@
+import { mongoose } from "../database";
+import { IBooking } from "../entities/Booking";
+
+// <# Booking: Representa uma reserva de quarto em um hotel. Atributos:
+
+// - id: Identificador único da reserva.
+// - checkin_date: Data de check-in da reserva.
+// - checkout_date: Data de checkout da reserva.
+// - gests: Quantidade de hóspedes.
+// - id_room: Identificador do quarto reservado.
+// - id_guest: Identificador do hóspede que fez a reserva.
+// - status: Status da reserva (por exemplo, "confirmada", "cancelada", "em andamento", "concluída").>
+
+const bookingSchema = new mongoose.Schema<IBooking>({
+    checkin_date: { type: Date, default: Date.now},
+    checkout_date: { type: Date, default: Date.now },
+    gests: {type: Number}
+})
