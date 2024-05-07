03c4c0996119f60a5c79ac69e1df6cf90501e9f6
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-05-07 20:31:07 -0300

    create 1 file and update 1 file

---
+++ src/models/GuestModel.ts
@@ -0,0 +1,14 @@
+import { mongoose } from "../database";
+import { IGuest } from "../entities/Guest";
+
+const guestSchema = new mongoose.Schema<IGuest>({
+    name: {Type: String},
+    cpf: {Type: String},
+    phone_number: {Type: Number},
+    email: {Type: String
+    },
+    password: {type: String},
+    bookings: {type: [mongoose.Types.ObjectId], ref: "Booking"}
+})
+
+export const Guest = mongoose.model("Guest", guestSchema)
--- src/models/RoomModel.ts
+++ src/models/RoomModel.ts
@@ -1,14 +1,13 @@
 import { mongoose } from "../database";
 import { IRoom } from "../entities/Room";
 
-
 const roomSchema = new mongoose.Schema<IRoom>({
-    number: {Type: Number, required: true},
-    type: {type: String, default: "individual"},
-    guest_capacity: {Type: Number},
-    daily_rate: {Type: Number},
-    photo: {Type: String},
-    status: {Type: String, default: "disponível"}
-})
+  number: { Type: Number, required: true },
+  type: { type: String, default: "individual" },
+  guest_capacity: { Type: Number },
+  daily_rate: { Type: Number },
+  photo: { Type: String },
+  status: { Type: String, default: "disponível" }
+});
 
-export const Room = mongoose.model("Room", roomSchema)
+export const Room = mongoose.model("Room", roomSchema);
